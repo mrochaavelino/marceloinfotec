e2bd10dc45485961791b01cf10ad5ba309cb9d11
Marcelo Rocha Avelino - Home <mrochaavelino@gmail.com>
2023-05-10 16:38:43 -0300

    version develop

---
+++ marceloinfotec/project/marceloinfotec/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+        <meta name="generator" content="Hugo 0.104.2">
+        <title>Marcelo - InfoTec</title>
+        
+        <!-- Bootstrap ext -->
+        <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.css"/>
+        
+        <!-- Custom style ext -->
+        <link rel="stylesheet" href="public/css/index.css"/>
+    </head>
+    <body>
+        
+            <div class="header">
+                <div class="fixed-square-purple"></div>
+                <div class="fixed-square-red"></div>
+            </div>
+           
+        <div class="">
+            
+        </div>
+        <div class="">
+            
+        </div>
+    </body>
+</html>
